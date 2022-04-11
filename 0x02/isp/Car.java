86d1f99efc1161425f5883df7180e42fa151f610
ELTON HENRIQUE <eltonhst34@gmail.com>
2022-04-10 23:24:23 -0300

    Create Car.java

---
+++ 0x02/isp/Car.java
@@ -0,0 +1,18 @@
+public class Car implements IVehicle, IVehicleCar, IMotorVehicle {
+
+	@Override
+	public void startVehicle() {
+
+	}
+
+	@Override
+	public void configureCar(String color, String year, double engine, int seats) {
+
+	}
+
+	@Override
+	public void kmPerLitre() {
+
+	}
+
+}
