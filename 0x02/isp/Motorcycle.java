142fee59bf77e0a0c8c3931ecbb74cd2c213fca0
ELTON HENRIQUE <eltonhst34@gmail.com>
2022-04-10 23:27:46 -0300

    Create Motorcycle.java

---
+++ 0x02/isp/Motorcycle.java
@@ -0,0 +1,19 @@
+public class Motorcycle  implements IVehicleMotorcycle, IMotorVehicle{
+
+	@Override
+	public void startVehicle() {
+		
+	}
+
+
+	@Override
+	public void configureMotorcycle(String color, String year, double engine) {
+		
+	}
+
+	@Override
+	public void kmPerLitre() {
+		
+	}
+
+}
