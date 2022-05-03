9ec599ffe4296e7dfa92c7b9cfc9b027e6a3f3c7
EltonCiandt <eltonhs@ciandt.com>
2022-05-02 22:25:17 -0300

    teste

---
+++ 0x04/convencao/User.java
@@ -0,0 +1,23 @@
+// classe users, representando nossa entidade
+
+class UsersModels {
+
+    int IdUser;
+
+    String Descricao;
+
+    final static int IDADE_MINIMA = 18;
+
+// função que coleta todos os users
+
+    public fetchUsers() { return [];}
+
+// função que coleta todos os users
+
+    public allUsers() {return [];}
+
+// função que coleta todos os users
+
+    public getUsers() {return [];}
+
+}
